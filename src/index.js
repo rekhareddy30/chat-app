d38998320339873b49da82d5654864d9b2a411a0
Mandala Rekha Devi <rekhareddy30@gmail.com>
2020-09-07 17:05:02 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const http = require("http")
+const express = require('express')
+const path = require('path')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log("New Web Socket Connection")
+})
+server.listen(port, () => {
+    console.log(`Server is UP & Running on post ${port}.`)
+})
